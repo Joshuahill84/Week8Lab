4dfe8dd6a9f536bea40df14449f42a51a7316e61
Josh Hill <joshuahill84@gmail.com>
2015-12-11 14:17:34 -0600

    Added Post Class

---
+++ Week8Lab.Api/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Week8Lab.Api.Models
+{
+    public class Post
+    {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Title { get; set; }
+            public string Text { get; set; }
+            public string URL { get; set; }
+            public DateTime PostTime { get; set; }
+    }
+}
